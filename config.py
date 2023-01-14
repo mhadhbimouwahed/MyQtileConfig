0739467012142eff00c342adaff70cfbf1782c46
mhadhbimouwahed <mhadhbi.mouwahed@ENICAR.u-carthage.tn>
2023-01-14 17:37:53 +0100

    added requirements.txt

--- config.py
+++ config.py
@@ -4,13 +4,11 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
+
 mod = "mod4"
 terminal = guess_terminal()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
